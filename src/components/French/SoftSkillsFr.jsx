955c43adf40a921be213736d10034d8644bca9f5
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-05-02 11:25:04 +0200

    modified and corrected softskils fr

--- src/components/French/SoftSkillsFr.jsx
+++ src/components/French/SoftSkillsFr.jsx
@@ -3,8 +3,7 @@ function SoftSkillsFr() {
 
         <div id="SoftSkills">
             <h2 className="overline">SoftSkills</h2>
-            <p>Férue de nouvelles technologies depuis septembre 2022 je réoriente en tant que développeuse J&apos;utilise régulierement ma permanente envie d&apos;apprendre, pour suivre des MOOC et webinaires notamment dans différents domaines.
-            </p>
+            <p>Férue de nouvelles technologies depuis septembre 2022 je me réoriente en tant que développeuse. J&apos;use de ma permanente envie d&apos;apprendre, pour suivre des MOOC et webinaires dans des domaines variés. </p>
         </div>
     )
 }
