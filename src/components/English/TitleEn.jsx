a5f9a1ba1f9793a8c1f503aed0006d099393ca62
@Delphine2669 <delphine.thevenet2669@gmail.com>
2023-05-23 22:38:40 +0200

    corrected grammar

--- src/components/English/TitleEn.jsx
+++ src/components/English/TitleEn.jsx
@@ -2,8 +2,8 @@ function TitleEn() {
     return (
         <div id="Title">
             <h1>Delphine Thevenet</h1>
-            <h4 className="titleJobName">Web & Web Mobile Dev / iOS Junior Dev</h4>
-            <h2 className="stage placement">Actively looking for a work placement starting from October 2nd to December 15th 2023</h2>
+            <h4 className="titleJobName">Mobile and Web Developer / iOS Junior Dev</h4>
+            <h2 className="stage internshop">Actively looking for an internship starting from October 2nd to December 15th 2023</h2>
       
         </div>
     )
--- src/components/French/TitleFr.jsx
+++ src/components/French/TitleFr.jsx
@@ -8,7 +8,7 @@ function TitleFr() {
         <div id="Title">
             <h1>Delphine Thevenet</h1>
             <h3 >Développeuse Web & Web Mobile / Dev iOS Junior</h3><br/>
-            <h2 className="stage placement">En recherche active de stage pour la période du 2 octobre au 15 décembre 2023</h2><br/>
+            <h2 className="stage internship">En recherche active de stage pour la période du 2 octobre au 15 décembre 2023</h2><br/>
             <h3><a href="/pdfs/CVDT2023.pdf" download onClick={handleDownload}>téléchargez mon CV en pdf</a></h3>
         </div>
     )
